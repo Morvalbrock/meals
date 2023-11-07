5291a05469cdedd66a679b8cb32907da4c5df966
Morvalbrock <agilansr222@gmail.com>
2023-11-07 16:02:36 +0530

    meals app started

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/categories.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: const CategoriesScreen(),
+    );
+  }
+}
