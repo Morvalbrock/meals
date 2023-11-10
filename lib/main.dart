4de1b3044c4075da388f7178bee0df97f2c8004f
Morvalbrock <agilansr222@gmail.com>
2023-11-10 16:08:41 +0530

    tab screen fav balance

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/tabs.dart';
 
 void main() {
   runApp(
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const TabScreen(),
     );
   }
 }
