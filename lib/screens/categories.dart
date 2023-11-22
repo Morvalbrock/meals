76f74b295a977b04d1224420161ee462ec907615
Morvalbrock <agilansr222@gmail.com>
2023-11-22 19:44:24 +0530

    explicit animation complete

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -78,11 +78,12 @@ class _CategoriesScreenState extends State<CategoriesScreen>
           ],
         ),
         builder: (contax, child) => SlideTransition(
-              position: _animationController.drive(
-                Tween(
-                  begin: const Offset(0, 0.3),
-                  end: const Offset(0, 0),
-                ),
+              position: Tween(
+                begin: const Offset(0, 0.3),
+                end: const Offset(0, 0),
+              ).animate(
+                CurvedAnimation(
+                    parent: _animationController, curve: Curves.easeInOut),
               ),
               child: child,
             ));
